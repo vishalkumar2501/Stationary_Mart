5f438074f0a3416d5538b201916802f09126c43c
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-06-12 23:38:09 +0530

    update code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,27 @@
-function Navbar() {
+const ProductCard = ({ name, price, image }) => {
   return (
-    <nav className="navbar">
-      <h2>Stationary Mart</h2>
+    <div className="bg-white rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl hover:-translate-y-2 transition duration-300">
+      <img
+        src={image}
+        alt={name}
+        className="h-52 w-full object-cover"
+      />
 
-      <ul className="nav-links">
-        <li>Home</li>
-        <li>Products</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+      <div className="p-5">
+        <h3 className="text-xl font-bold">
+          {name}
+        </h3>
+
+        <p className="text-blue-600 font-semibold mt-2">
+          ₹{price}
+        </p>
+
+        <button className="mt-4 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 cursor-pointer">
+          View Product
+        </button>
+      </div>
+    </div>
   );
-}
+};
 
-export default Navbar;
+export default ProductCard;
