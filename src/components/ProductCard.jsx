58747a1ba4fd1d51c62461576de104ddba812b73
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-06-12 23:40:08 +0530

    update code

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,22 @@
 const ProductCard = ({ name, price, image }) => {
   return (
-    <div className="bg-white rounded-2xl shadow-lg overflow-hidden hover:scale-105 transition duration-300">
+    <div className="bg-white rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl hover:-translate-y-2 transition duration-300">
       <img
         src={image}
         alt={name}
         className="h-52 w-full object-cover"
       />
 
-      <div className="p-4">
-        <h3 className="text-xl font-semibold">{name}</h3>
+      <div className="p-5">
+        <h3 className="text-xl font-bold">
+          {name}
+        </h3>
 
-        <p className="text-blue-600 font-bold mt-2">
+        <p className="text-blue-600 font-semibold mt-2">
           ₹{price}
         </p>
 
-        <button className="mt-4 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">
+        <button className="mt-4 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 cursor-pointer">
           View Product
         </button>
       </div>
